634738a727c235203b85fb7630e7e177f94504a0
Anders <anders @ iznogood.tech>
2020-03-01 18:41:32 +0100

    feedback url updated

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 VERSION = "0.3.0"
-CLI_PROXY_URL = "https://cli-proxy.unialt.no"
+CLI_PROXY_URL = "https://us-central1-getenv.cloudfunctions.net/getenvSendFeedback"
 CONFIG_FILE_PATH = os.path.join(os.path.expanduser("~"), ".config", "getenv.ini")
